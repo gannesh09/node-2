28b62867484a2213557b3c394b714cd384c51eca
gannesh09 <113293039+gannesh09@users.noreply.github.com>
2022-09-30 23:20:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,67 @@
-<!DOCTYPE html>
+// JavaScript source code
+const http = require("http");
+const fs = require("fs");
+var port = process.argv.slice(2)
+port = parseInt(String(port).slice(7))
+console.log(port);
+//fs.readFile("home.html", (err, home) => {
+//    console.log(home.toString());
+//});
+//fs.readFile("registration.html", (err, registration) => {
+//    console.log(registration.toString());
+//});
+//fs.readFile("home.html", (err, home) => {
+//    if (err) {
+//        throw err;
+//    }
+//    http
+//        .createServer((request, response) => {
+//            response.writeHeader(200, { "Content-Type": "text/html" });
+//            response.write(home);
+//            response.end();
+//        })
+//        .listen(3000);
+//});
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
 
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <title>My Application Form</title>
-</head>
-<body>
-    <h1>My Application Home</h1>
-    <a href="./project">Projects</a>
-</body>
-</html>
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    projectContent = project;
+});
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = registration;
+});
+http
+    .createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            case "/registration":
+                response.write(registrationContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+        }
+    })
+    .listen(port);
